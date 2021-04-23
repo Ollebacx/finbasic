d708b025fa9f84576a38dd114af3b73b6bc542f7
Ollebacx <dav.ollebac@gmail.com>
2021-04-23 23:30:31 +0100

    feat(Index): clean basic structure

--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,7 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import { Navbar } from '../components/Navbar'
-export default function Home() {
-  return (
+const Home = () => (
     <div className={styles.container}>
-      <Head>
-        <meta charSet='utf-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
-        <meta name='description' content='Description' />
-        <meta name='keywords' content='Keywords' />
-        <title>Finbasic</title>
-
-        <link rel='manifest' href='/manifest.json' />
-        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
-        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
-        <link rel='apple-touch-icon' href='/apple-icon.png'></link>
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        ></link>
-        <link
-          rel="preload"
-          as="style"
-          href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-        ></link>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          media="print"
-          onLoad="this.media='all'"
-        ></link>
-        <noscript>
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          ></link>
-        </noscript>
-        <meta name='theme-color' content='#444444' />
-      </Head>
-      <p1 className={'title'}> Finbasic </p1>
-      <Navbar />
       <main className={styles.main}>
 
         <div className={styles.grid}>
@@ -76,17 +34,7 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
-}
+
+  export default Home

3b60fc00853df8bede5779eff5cf1cb6b98ddc1b
Ollebacx <dav.ollebac@gmail.com>
2021-04-23 23:26:43 +0100

    feat(Header): create new component

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+const headerStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  color: "white",
+  width: "100%",
+  height: "60px"
+};
+
+const Header = () => (
+  <div style={headerStyle}>
+    <p1 className={'title'}> Finbasic </p1>
+  </div>
+);
+
+export default Header;

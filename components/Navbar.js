d2da8ec93a00ee78498c2f2e24dc9e738c835b1b
Ollebacx <dav.ollebac@gmail.com>
2021-04-23 13:28:41 +0100

    feat(Index, Navbar): clean initial index and add a new Navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export const Navbar = (params) => {
+  return (
+    <nav>
+      <a href='#'>Cuentas</a>
+      <a href='#'>Presupuesto</a>
+      <a href='#'>Categorías</a>
+      <a href='#'>Análisis</a>
+      <a href='#'>Mensual</a>
+      <div class='animation start-cuentas'></div>
+    </nav>
+  )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,50 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+import { Navbar } from '../components/Navbar'
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <meta charSet='utf-8' />
+        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
+        <meta name='description' content='Description' />
+        <meta name='keywords' content='Keywords' />
+        <title>Finbasic</title>
+
+        <link rel='manifest' href='/manifest.json' />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel='apple-touch-icon' href='/apple-icon.png'></link>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        ></link>
+        <link
+          rel="preload"
+          as="style"
+          href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          media="print"
+          onLoad="this.media='all'"
+        ></link>
+        <noscript>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+          ></link>
+        </noscript>
+        <meta name='theme-color' content='#444444' />
       </Head>
-
+      <p1 className={'title'}> Finbasic </p1>
+      <Navbar />
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>

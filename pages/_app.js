3939384fc435cc035c452c1779b8a49df03fe86e
Ollebacx <dav.ollebac@gmail.com>
2021-04-24 22:07:06 +0100

    feat(_app): add pages transitions package and implement in _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
 import '../styles/globals.css'
 import MainLayout from '../layouts/MainLayout'
-function MyApp({ Component, pageProps }) {
+import { PageTransition } from 'next-page-transitions'
+function MyApp({ Component, pageProps, router }) {
+  // TO DO: create a router query object instead of an array in NavItem
+  // const { enter, exit } = router.query;
+  const enter = router.query[0]
+  const exit = router.query[1]
   return (
-  <>
-    <MainLayout>
-      <Component {...pageProps} />
-    </MainLayout>
-  </>
+    <>
+      <MainLayout>
+        <PageTransition timeout={1200} classNames="page-transition">
+          <Component {...pageProps} key={router.route}/>
+        </PageTransition>
+        <style jsx global>{`
+          .page-transition-enter {
+            transform: translateX(${enter});
+          }
+          .page-transition-enter-active {
+            transition: transform 1s;
+            transform: translateX(0%);
+          }
+          .page-transition-exit {
+            transform: translateX(0%);
+
+          }
+          .page-transition-exit-active {
+            transform: translate(${exit});
+            transition: transform 1s;
+          }
+        `}</style>
+      </MainLayout>
+    </>
   )
 }
 
